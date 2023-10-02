e075c01ce98857927d6a6ed465407e15a86afadc
Valerij <v.ezhov@flyingbird.kz>
2023-10-02 06:11:41 +0600

    config

--- configs/config.py
+++ configs/config.py
@@ -1,9 +1,26 @@
 import os
+from dataclasses import dataclass
+from environs import Env
 
-API_TOKEN = 'your_token'
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+
 
 DATABASE = {
-    'dbname': os.environ.get('POSTGRES_DB', 'default_db_name'),
+    'database': os.environ.get('POSTGRES_DB', 'default_db_name'),
     'user': os.environ.get('POSTGRES_USER', 'default_user'),
     'password': os.environ.get('POSTGRES_PASSWORD', 'default_password'),
     'host': os.environ.get('POSTGRES_HOST', 'default_host'),
